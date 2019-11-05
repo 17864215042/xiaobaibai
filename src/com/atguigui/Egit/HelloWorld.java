715d216b063fec9a89b02b43b301c4f6bc7cedba
ZhiJie1 <1486326732@qq.com>
2019-11-05 18:43:37 +0800

    克隆

--- src/com/atguigui/Egit/HelloWorld.java
+++ src/com/atguigui/Egit/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("世界你好");
-		System.out.println("fajklsfjs12");
+		System.out.println("凤凰健康和覅女啊");
 	}
 
 }
