19b7786891bb399c7ad5684e0356743f9d954c9c
ZhiJie <1486326732@qq.com>
2019-11-05 19:36:42 +0800

    更新1

--- src/com/atguigui/Egit/HelloWorld.java
+++ src/com/atguigui/Egit/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("世界你好");
-		System.out.println("fajklsfjs");
+		System.out.println("fajklsfjs122");
 	}
 
 }
